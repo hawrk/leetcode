63e31e1dbcbc2027aaa86fafed936766af0bfabe
hawrkchen <chenhongwan@pudutech.com>
2022-02-15 11:14:17 +0800

    feat: fix format

--- 1.go
+++ 1.go
@@ -9,7 +9,7 @@ package main
 func twoSum(nums []int, target int) []int {
 	for k1, v1 := range nums {
 		for k2, v2 := range nums {
-			if v1 + v2 == target {
+			if v1+v2 == target {
 				return []int{k1, k2}
 			}
 		}
